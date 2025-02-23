c15f74b886c8b85cf6e073033ec6d5cf09662143
ezeksuresh03 <ezeksuresh03@gmail.com>
2025-02-23 23:56:05 +0800

    Created UI for extension

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -1,10 +1,15 @@
-
+import React from "react"
+import { Button } from "@/components/ui/button"
+import { ScanQrCode } from "lucide-react"
 
 function Popup() {
 
     return (
-        <div>
-            <p>Hola</p>
+        <div className="w-[300px] p-4 flex flex-col items-center gap-4">
+            <h1 className="text-center">Web QR Scanner</h1>
+            <Button className="w-full"> 
+                <ScanQrCode className="w-5 h-5 mr-2"/> Scan QR Code
+            </Button>
         </div>
     )
 }

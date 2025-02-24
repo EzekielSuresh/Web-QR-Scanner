fdb864b05fa8eb5a1e5dd800de525cd9cf165cd2
ezeksuresh03 <ezeksuresh03@gmail.com>
2025-02-25 00:23:44 +0800

    Fixed tailwindcss issue

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -1,14 +1,16 @@
 import React from "react"
 import { Button } from "@/components/ui/button"
 import { ScanQrCode } from "lucide-react"
+import '../index.css'
 
 function Popup() {
 
     return (
         <div className="w-[300px] p-4 flex flex-col items-center gap-4">
-            <h1 className="text-center">Web QR Scanner</h1>
-            <Button className="w-full"> 
-                <ScanQrCode className="w-5 h-5 mr-2"/> Scan QR Code
+            <h1 className="text-center font-semibold">Web QR Scanner</h1>
+            <Button className="w-full font-semibold"> 
+                <ScanQrCode className="w-5 h-5 mr-2"/> 
+                <span className="w-[120px] text-lg">Scan QR Code</span>
             </Button>
         </div>
     )

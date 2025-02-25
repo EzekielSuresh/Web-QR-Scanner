95fd42155abe43259f5b047e6ea9494205787b1e
ezeksuresh03 <ezeksuresh03@gmail.com>
2025-02-25 23:51:37 +0800

    Added event listeners

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -1,17 +1,34 @@
-import React from "react"
 import { Button } from "@/components/ui/button"
 import { ScanQrCode } from "lucide-react"
+import { useState } from "react"
 import '../index.css'
 
 function Popup() {
 
+    const [result, setResult] = useState("")
+
+    const handleClick = () => {
+        chrome.runtime.sendMessage({ action: "capture_screen"}, (response) => {
+
+            chrome.runtime.onMessage.addListener((message) => {
+
+                if ( message.action == "qr_result" ) {
+                    setResult(response.result)
+                } else {
+                    setResult("No QR Code detected.")
+                }
+            })
+        })
+    } 
+
     return (
         <div className="w-[300px] p-4 flex flex-col items-center gap-4">
             <h1 className="text-center font-semibold">Web QR Scanner</h1>
-            <Button className="w-full font-semibold"> 
-                <ScanQrCode className="w-5 h-5 mr-2"/> 
-                <span className="w-[120px] text-lg">Scan QR Code</span>
+            <Button onClick={handleClick} className="w-full font-semibold"> 
+                <ScanQrCode className="w-5 h-5"/> 
+                <span className="text-md">Scan QR Code</span>
             </Button>
+            <p>URL: {result}</p>
         </div>
     )
 }

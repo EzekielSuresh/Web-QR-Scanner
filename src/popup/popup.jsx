ae04a5ab62603d4401b294dcfb4a82bd816bcba7
ezeksuresh03 <ezeksuresh03@gmail.com>
2025-03-30 13:58:33 +0800

    Added support for multi QR scanning

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -13,47 +13,107 @@ import {
     AlertTitle
  } from "@/components/ui/alert";
 import { ScanQrCode } from "lucide-react"
-import { CircleAlert } from "lucide-react";
-import '@mariusbongarts/previewbox/dist/link/index';
-import { useState } from "react"
-import jsQR from "jsqr"
+import { CircleAlert } from "lucide-react"
+import '@mariusbongarts/previewbox/dist/link/index'
+import { useState, useRef } from "react"
 import '../index.css'
 
 function Popup() {
 
-    const [result, setResult] = useState("")
+    const [result, setResult] = useState([])
     const [error, setError] = useState(null)
+    const [capturedImage, setCapturedImage] = useState(null)
+    let masks = useRef([])
 
-    const handleClick = () => {
-        chrome.runtime.sendMessage({ action: "capture_screen"}, (response) => {
+    const handleClick = async () => {
+        setError(null)
+        setCapturedImage(null)
+        setResult([])
+
+        chrome.runtime.sendMessage({ action: "capture_screen"}, async (response) => {
             if (!response || response.result != "success") {
                 setError("Failed to capture screen.")
                 return
             }
 
+            setCapturedImage(response.image)
+
             const img = new Image()
             img.src = response.image
+            await img.decode()
             
-            img.onload = () => {
-                const canvas = document.createElement("canvas")
-                const ctx = canvas.getContext("2d", { alpha: false })
+            let canvas = document.createElement("canvas")
+            const ctx = canvas.getContext("2d", { alpha: false, willReadFrequently: true })
 
-                canvas.width = img.width
-                canvas.height = img.height
-                ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+            canvas.width = img.width
+            canvas.height = img.height
+            ctx.imageSmoothingEnabled = false
+            ctx.mozImageSmoothingEnabled = false
+            ctx.webkitImageSmoothingEnabled = false
+            ctx.msImageSmoothingEnabled = false
 
-                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
 
-                const qrCode = jsQR(imageData.data, imageData.width, imageData.height)
+            // let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
-                if (qrCode) {
-                    setResult(qrCode.data)
-                } else {
-                    setError("No QR Code found.")
+            let qrCodeList = []
+            let tries = 0
+            const maxTries = 5
+
+            const { BrowserMultiFormatReader } = await import('@zxing/browser')
+            const codeReader = new BrowserMultiFormatReader()
+
+            while ( tries < maxTries ) {
+
+                let qrCode = ""
+
+                try {
+                    qrCode = await codeReader.decodeFromCanvas(canvas)
+                } catch (error) {
+                    break
                 }
 
+                qrCodeList.push(qrCode.text)
+
+                let topLeftCorner = qrCode.resultPoints[0]
+                let topRightCorner = qrCode.resultPoints[1]
+                let bottomRightCorner = qrCode.resultPoints[2]
+                let width = Math.abs(topRightCorner.x - topLeftCorner.x)
+                let height = Math.abs(topRightCorner.y - bottomRightCorner.y)
+                await maskQRCode(ctx, img, canvas.width, canvas.height, topLeftCorner.x, topLeftCorner.y, width, height)
+
+                tries++
             }
+            
+
+            if (qrCodeList.length > 0) {
+                setResult(qrCodeList)
+            } else {
+                setError("No QR Code found.")
+            }            
+                
+
         })
+    }
+
+    async function maskQRCode(ctx, img, width, height, x, y, maskWidth, maskHeight) {
+        const padding = 10
+        masks.current.push({
+            x: x - padding, 
+            y: y - padding, 
+            maskWidth: maskWidth + 2 * padding, 
+            maskHeight: maskHeight + 2 * padding
+        })
+
+        ctx.clearRect(0, 0, width, height)
+    
+        ctx.drawImage(img, 0, 0, width, height)
+    
+        ctx.fillStyle = "white";
+        masks.current.forEach(mask => {
+            ctx.fillRect(mask.x, mask.y, mask.maskWidth, mask.maskHeight);
+        })
+
     }
 
     return (
@@ -69,20 +129,27 @@ function Popup() {
                         <CircleAlert className="h-4 w-4"/>
                         <AlertTitle>{error}</AlertTitle>
                     </Alert>}
-
-                    {!result ? (
+                    {capturedImage && (
+                        <div className="mb-4">
+                            <img src={capturedImage} alt="Captured Screen" className="w-full rounded-lg shadow-md"/>
+                        </div>
+                    )}
+                    {!result || result.length == 0 ? (
                         <div></div>
                     ) : (
                         <div className="w-full">
-                            <previewbox-link 
-                                href={result}
-                                style={{ 
-                                    display: "block", 
-                                    width: "300px", 
-                                    maxWidth: "100%" 
-                                }}
-                                light
-                            ></previewbox-link>
+                            {result.map((item, index) => (
+                                <previewbox-link 
+                                    key={index}
+                                    href={item}
+                                    style={{ 
+                                        display: "block", 
+                                        width: "300px", 
+                                        maxWidth: "100%" 
+                                    }}
+                                    light
+                                ></previewbox-link>
+                            ))}
                         </div>
                     )}
                 </CardContent>
